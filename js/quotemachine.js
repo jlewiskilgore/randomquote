690b68de68c26da79410d2d5aac5fe683cf379b2
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-02 13:31:48 -0500

    Make GET request to forismatic API for random quote

--- js/quotemachine.js
+++ js/quotemachine.js
@@ -1,6 +1,14 @@
 //TODO: Function will quote Quote API and return string to render
 function getQuote() {
-	alert("getQuote button pressed");
+	//alert("getQuote button pressed");
+
+	$.ajax({
+		url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en",
+		type: 'GET',
+		success: function(results) {
+			alert(results.quoteText+results.quoteAuthor);
+		}
+	});
 }
 
 function tweetQuote() {
