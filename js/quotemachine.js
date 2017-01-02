6568de5b7f6d636e10251f7e4641bc64ed90acbf
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-02 13:42:12 -0500

    Update page content with API response

--- js/quotemachine.js
+++ js/quotemachine.js
@@ -1,12 +1,15 @@
 //TODO: Function will quote Quote API and return string to render
 function getQuote() {
 	//alert("getQuote button pressed");
-
+	var qText = document.getElementById("randQuoteText");
+	var qAuth = document.getElementById("randQuoteAuthor");
 	$.ajax({
 		url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en",
 		type: 'GET',
 		success: function(results) {
-			alert(results.quoteText+results.quoteAuthor);
+			//alert(results.quoteText+results.quoteAuthor);
+			qText.innerHTML = '"' + results.quoteText + '"';
+			qAuth.innerHTML = '- ' + results.quoteAuthor;
 		}
 	});
 }
