0b39d5fd8180ed4bb3fbcde6a3b886795547453f
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-01 22:13:04 -0500

    add placeholders for buttons and text

---
+++ js/quotemachine.js
@@ -0,0 +1,8 @@
+//TODO: Function will quote Quote API and return string to render
+function getQuote() {
+	alert("getQuote button pressed");
+}
+
+function tweetQuote() {
+	alert("tweetQuote button pressed");
+}
